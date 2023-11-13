14528f7b77c73e30cce983428ddbc62266f21919
karosi12 <adekayor@gmail.com>
2023-11-13 19:57:34 +0200

    ch: clean up setup

--- script.groovy
+++ script.groovy
@@ -9,13 +9,6 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    /**  docker image push to docker hub
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t karosi12/demo-app:${IMAGE_NAME} ."
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push karosi12/demo-app:${IMAGE_NAME}"
-    }
-    */
     withCredentials([usernamePassword(credentialsId: 'ecr-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "docker build -t ${DOCKER_REPO}:${IMAGE_NAME} ."
         sh "echo $PASS | docker login -u $USER --password-stdin ${DOCKER_REPO_SERVER}"
