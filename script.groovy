a864b7bbbdeae40fa7dccacf9110af89582f455c
karosi12 <adekayor@gmail.com>
2023-12-12 16:27:38 +0200

    feat: run ansible playbook

--- script.groovy
+++ script.groovy
@@ -23,7 +23,7 @@ def executeAnsible() {
     withCredentials([sshUserPrivateKey(credentialsId: 'ansible-server-key', keyFileVariable: 'keyfile', usernameVariable: 'user')]) {
         remote.user = user
         remote.identityFile = keyfile
-        sshCommand remote: remote, command: "ls -l"
+        sshCommand remote: remote, command: "ansible-playbook my-playbook.yaml"
     }
 }
 
